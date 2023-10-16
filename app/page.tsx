6b3f4a9b38e7be4ef4f00077479f2621a38d343e
awdfaf <awdfaf@kakao.com>
2023-10-16 14:23:09 +0900

    feat: 헤더 임시 디자인 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,70 @@
-'use client'
 
-import Logout from '@/components/layout/ui/logout'
+
+
 import React from 'react'
-import { Link } from "@nextui-org/react";
-import Images from '@/components/page/main/(image)/Images';
+
 
 
 
 export default function Home() {
   return (
     <>
-
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
+      <h1>adasd</h1>
     </>
   )
 }
