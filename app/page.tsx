87142c6a8e7b31d61853360efc71b1eafe880751
awdfaf <awdfaf@kakao.com>
2023-10-17 12:08:52 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-
-
-
+import CategoryMenu from '@/components/page/main/(menu)/CategoryMenu'
 import MainPageSwiper from '@/components/widget/swiper/MainPageSwiper'
-
 import React from 'react'
-
-
-
 
 export default function Home() {
   return (
@@ -15,8 +9,11 @@ export default function Home() {
       <h1> adasd</h1 >
       <h1>adasd</h1>
       <h1>adasd</h1>
+      <CategoryMenu />
       <h1>adasd</h1>
       <h1>adasd</h1>
+      <p className="sp_tit1 fw500">서비스 동의 관리</p>
+      <p className="sp_txt4">신세계포인트의 다양한 서비스 혜택을 받을 수 있습니다.</p>
       <h1>adasd</h1>
       <h1>adasd</h1>
       <h1>adasd</h1>
