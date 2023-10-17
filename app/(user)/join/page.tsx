46a25967f50694518412a00828fca19c42b26943
jskim4695 <jskim4695@github.com>
2023-10-17 16:42:30 +0900

    Feat: cert, join page 생성

---
+++ app/(user)/join/page.tsx
@@ -0,0 +1,17 @@
+import FormArea from '@/components/page/signup/FormArea'
+import React from 'react'
+
+function join() {
+    return (
+        <>
+            <div className='mt-[20px] text-2xl text-center'>
+                정보입력
+            </div>
+            <div>
+                <FormArea />
+            </div>
+        </>
+    )
+}
+
+export default join
