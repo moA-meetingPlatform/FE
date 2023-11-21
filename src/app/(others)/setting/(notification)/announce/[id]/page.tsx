2c8cdb27af9b5e17309f4bd4a267963001db57b3
awdfaf <awdfaf@kakao.com>
2023-11-21 16:15:09 +0900

    test

--- src/app/(others)/setting/(notification)/announce/[id]/page.tsx
+++ src/app/(others)/setting/(notification)/announce/[id]/page.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+// import React from "react";
 
-export default async function announcePost(props:any){
-  const res = await fetch(`http://localhost:9999/announces/${props.params.id}`)
-  const announce = await res.json();
+// export default async function announcePost(props:any){
+//   const res = await fetch(`http://localhost:9999/announces/${props.params.id}`)
+//   const announce = await res.json();
+//   return (
+//     <>
+//     <main className="mt-16">
+//       <h2 className="font-semibold">{announce.title}</h2>
+//       <h1 className="text-sm">{announce.date}</h1>
+//       {announce.body}
+//     </main>
+//     </>
+//   )
+// }
+import React from 'react'
+
+export default function page() {
   return (
-    <>
-    <main className="mt-16">
-      <h2 className="font-semibold">{announce.title}</h2>
-      <h1 className="text-sm">{announce.date}</h1>
-      {announce.body}
-    </main>
-    </>
+    <div>page</div>
   )
 }
